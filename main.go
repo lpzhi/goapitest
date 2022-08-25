9ed9012fceadb0fea4013a750a7133ec61bc932f
lupeng <1535933394@qq.com>
2022-08-25 11:37:50 +0800

    t

--- main.go
+++ main.go
@@ -4,9 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"goapitest/bootstrap"
-
 	bstconfig "goapitest/config"
-	config "goapitest/pkg/config"
+	"goapitest/pkg/config"
+	"log"
 
 	"github.com/gin-gonic/gin"
 )
@@ -21,12 +21,13 @@ func main() {
 	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
 	flag.Parse()
 	config.InitConfig(env)
-
+	bstconfig.Initialize()
 	r := gin.New()
 	bootstrap.SetupRoute(r)
 
-	err := r.Run(":" + config.GetString("app.port"))
+	err := r.Run(":" + config.Get("app.port"))
 	if err != nil {
 		fmt.Println(err.Error())
 	}
+	log.Fatalln("test")
 }
